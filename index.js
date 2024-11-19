64efe86b65e1f6997f426897aa6bd75c8a767c05
athalialimbo <limboalthea8@gmail.com>
2024-11-19 10:49:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-//console.log(fetch('https://encryptedhttps://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRibFlYmTMyAhYMpyQKWhk5WOFtJ2JlGc_SYMBcUO2dC1-0Pznn-tbn3.gstatic.com/images?q=tbn:ANd9GcQuSibRrzurJuEqRT_Xz4P3NkNOLyteKt6sYQJI5hEeQdP88P2znoKqjntR-93CYeySK-wRWelwTImG1q0kPpLZ46hyGNsbTejsqFYVag')
-    
-   // .then(response => {
-      //  console.log(response)
-      //  return response.blob();
-    //})
-
-
-    //.then(blob  =>{
-      //  console.log(blob)
-       // document.querySelector('#myImage').src=URL.createObjectURL(blob)
-    //})
-
-   // .catch(error =>{
-   //     cconsole.log(error)
-   // })
-//)
-
-
-//id selector
-const content=document.querySelector("#content")
+const content = document.querySelector('#content')
 
 //loading page
 window.addEventListener("load", () =>{
@@ -26,23 +6,23 @@ window.addEventListener("load", () =>{
 })
 
 function getUsers(){
-    let html=""
+    let html = ""
+    // fetch("http://localhost:1804/api/hello", { mode: "cors" })
+    fetch("https://bscs3a-api-crud-22f1.onrender.com/api/hello",{mode: "cors"})
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
 
-    fetch("https://bscs3a-crud-api.onrender.com/api/members" , {mode: "cors"})
-    .then((response) =>{
-        console.log(response)
-        return response.json()
-    })
-    .then((data)=>{
-        console.log(data)
-        data.forEach((element) =>{
-            html += `<li>${element.first_name} ${element.last_name}</li>`
-        })
+      .then((data) => {
+        console.log(data);
+        data.forEach((element) => {
+          html += `<li>${element.first_name} ${element.last_name}</li>`;
+        });
+        content.innerHTML = html;
+      })
 
-        content.innerHTML = html
-    })
-    .catch((error) =>{
-        console.log(error)
-    })
-
+      .catch((error) => {
+        console.log(error);
+      });
 }
